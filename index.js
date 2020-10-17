641fab486915adbcdfb05b4f772442dd1bcfa57f
Robson Antonio Lima Mendonça <rob.ams@gmail.com>
2020-10-17 17:17:39 -0300

    Update index.js
    correção do endpoint api: https://valida-spring-boot-robson.herokuapp.com/

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://valida-spring-boot-robson.herokuapp.com//validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
